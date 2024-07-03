2749fba2fb350bd0344c54f30d812e7942451e4f
tomaslm000 <84929944+tomas-lm@users.noreply.github.com>
2024-07-03 13:46:51 -0300

    adição de testes para 100%

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -16,10 +16,14 @@ test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
-  const perguntas = modelo.listar_perguntas(); 
+  const perguntas = modelo.listar_perguntas();
   expect(perguntas.length).toBe(3);
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
+  expect(perguntas[2].texto).toBe('3 + 3 = ?');
+  expect(perguntas[0].num_respostas).toBe(0);
+  expect(perguntas[1].num_respostas).toBe(0);
   expect(perguntas[2].num_respostas).toBe(0);
-  expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+  expect(perguntas[1].id_pergunta).toBe(perguntas[0].id_pergunta + 1);
+  expect(perguntas[2].id_pergunta).toBe(perguntas[1].id_pergunta + 1);
 });
